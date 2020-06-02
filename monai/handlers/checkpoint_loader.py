39473e31d289d626bd8da20a02c69c1d58f2f73d
Benjamin Gorman <benjamin-gorman@uiowa.edu>
2020-06-02 11:50:03 -0500

    remove (str) from docstring, add type hints
    Co-authored-by: Cameron Trentz <cameron-trentz@uiowa.edu>

--- monai/handlers/checkpoint_loader.py
+++ monai/handlers/checkpoint_loader.py
@@ -26,12 +26,12 @@ class CheckpointLoader:
     And also can restore training session if load the state_dict of Ignite engine.
 
     Args:
-        load_path (str): the file path of checkpoint, it should be a PyTorch pth file.
+        load_path: the file path of checkpoint, it should be a PyTorch pth file.
         load_dict (dict): target objects that load checkpoint to. examples::
 
             {'network': net, 'optimizer': optimizer, 'engine', engine}
 
-        name (str): identifier of logging.logger to use, if None, defaulting to ``engine.logger``.
+        name: identifier of logging.logger to use, if None, defaulting to ``engine.logger``.
 
     """
 
