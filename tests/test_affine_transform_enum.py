03e4cb7d9c9730d6cc5f68197df3319e864c63b3
Cameron Trentz <cameron-trentz@uiowa.edu>
2020-06-05 14:29:10 -0500

    Add InterpolationMode enum test
    Co-authored-by: Benjamin Gorman <benjamin-gorman@uiowa.edu>

---
+++ tests/test_affine_transform_enum.py
@@ -0,0 +1,153 @@
+# Copyright 2020 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+import torch
+from parameterized import parameterized
+
+from monai.networks.layers import AffineTransform
+from monai.networks.utils import normalize_transform, to_norm_affine
+from monai.utils.enum import InterpolationMode
+
+
+class TestNormTransformEnum(unittest.TestCase):
+    def test_affine_transform_2d(self):
+        t = np.pi / 3
+        affine = [[np.cos(t), -np.sin(t), 0], [np.sin(t), np.cos(t), 0], [0, 0, 1]]
+        affine = torch.as_tensor(affine, device=torch.device("cpu:0"), dtype=torch.float32)
+        image = torch.arange(24).view(1, 1, 4, 6).to(device=torch.device("cpu:0"))
+        xform = AffineTransform((3, 4), padding_mode="border", align_corners=True, mode=InterpolationMode.BILINEAR)
+        out = xform(image, affine)
+        out = out.detach().cpu().numpy()
+        expected = [
+            [
+                [
+                    [7.1525574e-07, 4.9999994e-01, 1.0000000e00, 1.4999999e00],
+                    [3.8660259e00, 1.3660253e00, 1.8660252e00, 2.3660252e00],
+                    [7.7320518e00, 3.0358994e00, 2.7320509e00, 3.2320507e00],
+                ]
+            ]
+        ]
+        np.testing.assert_allclose(out, expected, atol=1e-5)
+
+        if torch.cuda.is_available():
+            affine = torch.as_tensor(affine, device=torch.device("cuda:0"), dtype=torch.float32)
+            image = torch.arange(24).view(1, 1, 4, 6).to(device=torch.device("cuda:0"))
+            xform = AffineTransform(padding_mode="border", align_corners=True, mode=InterpolationMode.BILINEAR)
+            out = xform(image, affine, (3, 4))
+            out = out.detach().cpu().numpy()
+            expected = [
+                [
+                    [
+                        [7.1525574e-07, 4.9999994e-01, 1.0000000e00, 1.4999999e00],
+                        [3.8660259e00, 1.3660253e00, 1.8660252e00, 2.3660252e00],
+                        [7.7320518e00, 3.0358994e00, 2.7320509e00, 3.2320507e00],
+                    ]
+                ]
+            ]
+            np.testing.assert_allclose(out, expected, atol=1e-4)
+
+    def test_affine_transform_3d(self):
+        t = np.pi / 3
+        affine = [[1, 0, 0, 0], [0.0, np.cos(t), -np.sin(t), 0], [0, np.sin(t), np.cos(t), 0], [0, 0, 0, 1]]
+        affine = torch.as_tensor(affine, device=torch.device("cpu:0"), dtype=torch.float32)
+        image = torch.arange(48).view(2, 1, 4, 2, 3).to(device=torch.device("cpu:0"))
+        xform = AffineTransform((3, 4, 2), padding_mode="border", align_corners=False, mode=InterpolationMode.BILINEAR)
+        out = xform(image, affine)
+        out = out.detach().cpu().numpy()
+        expected = [
+            [
+                [
+                    [[0.00000006, 0.5000001], [2.3660254, 1.3660254], [4.732051, 2.4019241], [5.0, 3.9019237]],
+                    [[6.0, 6.5], [8.366026, 7.3660254], [10.732051, 8.401924], [11.0, 9.901924]],
+                    [[12.0, 12.5], [14.366026, 13.366025], [16.732052, 14.401924], [17.0, 15.901923]],
+                ]
+            ],
+            [
+                [
+                    [[24.0, 24.5], [26.366024, 25.366024], [28.732052, 26.401924], [29.0, 27.901924]],
+                    [[30.0, 30.5], [32.366028, 31.366026], [34.732048, 32.401924], [35.0, 33.901924]],
+                    [[36.0, 36.5], [38.366024, 37.366024], [40.73205, 38.401924], [41.0, 39.901924]],
+                ]
+            ],
+        ]
+        np.testing.assert_allclose(out, expected, atol=1e-4)
+
+        if torch.cuda.is_available():
+            affine = torch.as_tensor(affine, device=torch.device("cuda:0"), dtype=torch.float32)
+            image = torch.arange(48).view(2, 1, 4, 2, 3).to(device=torch.device("cuda:0"))
+            xform = AffineTransform(padding_mode="border", align_corners=False, mode=InterpolationMode.BILINEAR)
+            out = xform(image, affine, (3, 4, 2))
+            out = out.detach().cpu().numpy()
+            expected = [
+                [
+                    [
+                        [[0.00000006, 0.5000001], [2.3660254, 1.3660254], [4.732051, 2.4019241], [5.0, 3.9019237]],
+                        [[6.0, 6.5], [8.366026, 7.3660254], [10.732051, 8.401924], [11.0, 9.901924]],
+                        [[12.0, 12.5], [14.366026, 13.366025], [16.732052, 14.401924], [17.0, 15.901923]],
+                    ]
+                ],
+                [
+                    [
+                        [[24.0, 24.5], [26.366024, 25.366024], [28.732052, 26.401924], [29.0, 27.901924]],
+                        [[30.0, 30.5], [32.366028, 31.366026], [34.732048, 32.401924], [35.0, 33.901924]],
+                        [[36.0, 36.5], [38.366024, 37.366024], [40.73205, 38.401924], [41.0, 39.901924]],
+                    ]
+                ],
+            ]
+            np.testing.assert_allclose(out, expected, atol=1e-4)
+
+    def test_ill_affine_transform(self):
+        with self.assertRaises(ValueError):  # image too small
+            t = np.pi / 3
+            affine = [[1, 0, 0, 0], [0.0, np.cos(t), -np.sin(t), 0], [0, np.sin(t), np.cos(t), 0], [0, 0, 0, 1]]
+            affine = torch.as_tensor(affine, device=torch.device("cpu:0"), dtype=torch.float32)
+            xform = AffineTransform((3, 4, 2), padding_mode="border", align_corners=False, mode=InterpolationMode.BILINEAR)
+            xform(torch.as_tensor([1, 2, 3]), affine)
+
+        with self.assertRaises(ValueError):  # output shape too small
+            t = np.pi / 3
+            affine = [[1, 0, 0, 0], [0.0, np.cos(t), -np.sin(t), 0], [0, np.sin(t), np.cos(t), 0], [0, 0, 0, 1]]
+            affine = torch.as_tensor(affine, device=torch.device("cpu:0"), dtype=torch.float32)
+            image = torch.arange(48).view(2, 1, 4, 2, 3).to(device=torch.device("cpu:0"))
+            xform = AffineTransform((3, 4), padding_mode="border", align_corners=False, mode=InterpolationMode.BILINEAR)
+            xform(image, affine)
+
+        with self.assertRaises(ValueError):  # incorrect affine
+            t = np.pi / 3
+            affine = [[1, 0, 0, 0], [0.0, np.cos(t), -np.sin(t), 0], [0, np.sin(t), np.cos(t), 0], [0, 0, 0, 1]]
+            affine = torch.as_tensor(affine, device=torch.device("cpu:0"), dtype=torch.float32)
+            affine = affine.unsqueeze(0).unsqueeze(0)
+            image = torch.arange(48).view(2, 1, 4, 2, 3).to(device=torch.device("cpu:0"))
+            xform = AffineTransform((2, 3, 4), padding_mode="border", align_corners=False, mode=InterpolationMode.BILINEAR)
+            xform(image, affine)
+
+        with self.assertRaises(ValueError):  # batch doesn't match
+            t = np.pi / 3
+            affine = [[1, 0, 0, 0], [0.0, np.cos(t), -np.sin(t), 0], [0, np.sin(t), np.cos(t), 0], [0, 0, 0, 1]]
+            affine = torch.as_tensor(affine, device=torch.device("cpu:0"), dtype=torch.float32)
+            affine = affine.unsqueeze(0)
+            affine = affine.repeat(3, 1, 1)
+            image = torch.arange(48).view(2, 1, 4, 2, 3).to(device=torch.device("cpu:0"))
+            xform = AffineTransform((2, 3, 4), padding_mode="border", align_corners=False, mode=InterpolationMode.BILINEAR)
+            xform(image, affine)
+
+        with self.assertRaises(ValueError):  # wrong affine
+            affine = torch.as_tensor([[1, 0, 0, 0], [0, 0, 0, 1]])
+            image = torch.arange(48).view(2, 1, 4, 2, 3).to(device=torch.device("cpu:0"))
+            xform = AffineTransform((2, 3, 4), padding_mode="border", align_corners=False, mode=InterpolationMode.BILINEAR)
+            xform(image, affine)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_rand_elasticd_enum.py
@@ -0,0 +1,165 @@
+# Copyright 2020 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+import torch
+from parameterized import parameterized
+
+from monai.transforms import Rand3DElasticd
+from monai.utils.enums import InterpolationMode
+
+TEST_CASES = [
+    [
+        {
+            "keys": ("img", "seg"),
+            "magnitude_range": (0.3, 2.3),
+            "sigma_range": (1.0, 20.0),
+            "prob": 0.0,
+            "as_tensor_output": False,
+            "device": None,
+            "spatial_size": (2, 2, 2),
+            "mode": InterpolationMode.BILINEAR
+        },
+        {"img": torch.ones((2, 3, 3, 3)), "seg": torch.ones((2, 3, 3, 3))},
+        np.ones((2, 2, 2, 2)),
+    ],
+    [
+        {
+            "keys": ("img", "seg"),
+            "magnitude_range": (0.3, 0.3),
+            "sigma_range": (1.0, 2.0),
+            "prob": 0.9,
+            "as_tensor_output": False,
+            "device": None,
+            "spatial_size": (2, 2, 2),
+            "mode": InterpolationMode.BILINEAR
+        },
+        {"img": torch.arange(27).reshape((1, 3, 3, 3)), "seg": torch.arange(27).reshape((1, 3, 3, 3))},
+        np.array([[[[3.2385552, 4.753422], [7.779232, 9.286472]], [[16.769115, 18.287868], [21.300673, 22.808704]]]]),
+    ],
+    [
+        {
+            "keys": ("img", "seg"),
+            "magnitude_range": (0.3, 0.3),
+            "sigma_range": (1.0, 2.0),
+            "prob": 0.9,
+            "rotate_range": [1, 1, 1],
+            "as_tensor_output": False,
+            "device": None,
+            "spatial_size": (2, 2, 2),
+            "mode": InterpolationMode.BILINEAR,
+        },
+        {"img": torch.arange(27).reshape((1, 3, 3, 3)), "seg": torch.arange(27).reshape((1, 3, 3, 3))},
+        np.array([[[[1.6566806, 7.695548], [7.4342523, 13.580086]], [[11.776854, 18.669481], [18.396517, 21.551771]]]]),
+    ],
+    [
+        {
+            "keys": ("img", "seg"),
+            "mode": (InterpolationMode.BILINEAR, InterpolationMode.NEAREST),
+            "magnitude_range": (0.3, 0.3),
+            "sigma_range": (1.0, 2.0),
+            "prob": 0.9,
+            "rotate_range": [1, 1, 1],
+            "as_tensor_output": True,
+            "device": torch.device("cpu:0"),
+            "spatial_size": (2, 2, 2),
+        },
+        {"img": torch.arange(27).reshape((1, 3, 3, 3)), "seg": torch.arange(27).reshape((1, 3, 3, 3))},
+        {
+            "img": torch.tensor(
+                [[[[1.6566806, 7.695548], [7.4342523, 13.580086]], [[11.776854, 18.669481], [18.396517, 21.551771]]]]
+            ),
+            "seg": torch.tensor([[[[1.0, 11.0], [7.0, 17.0]], [[9.0, 19.0], [15.0, 25.0]]]]),
+        },
+    ],
+    [
+        {
+            "keys": ("img", "seg"),
+            "mode": [InterpolationMode.BILINEAR, InterpolationMode.NEAREST],
+            "magnitude_range": (0.3, 0.3),
+            "sigma_range": (1.0, 2.0),
+            "prob": 0.9,
+            "rotate_range": [1, 1, 1],
+            "as_tensor_output": True,
+            "device": torch.device("cpu:0"),
+            "spatial_size": (2, 2, 2),
+        },
+        {"img": torch.arange(27).reshape((1, 3, 3, 3)), "seg": torch.arange(27).reshape((1, 3, 3, 3))},
+        {
+            "img": torch.tensor(
+                [[[[1.6566806, 7.695548], [7.4342523, 13.580086]], [[11.776854, 18.669481], [18.396517, 21.551771]]]]
+            ),
+            "seg": torch.tensor([[[[1.0, 11.0], [7.0, 17.0]], [[9.0, 19.0], [15.0, 25.0]]]]),
+        },
+    ],
+    [
+        {
+            "keys": ("img", "seg"),
+            "mode": ("bilinear", InterpolationMode.NEAREST),
+            "magnitude_range": (0.3, 0.3),
+            "sigma_range": (1.0, 2.0),
+            "prob": 0.9,
+            "rotate_range": [1, 1, 1],
+            "as_tensor_output": True,
+            "device": torch.device("cpu:0"),
+            "spatial_size": (2, 2, 2),
+        },
+        {"img": torch.arange(27).reshape((1, 3, 3, 3)), "seg": torch.arange(27).reshape((1, 3, 3, 3))},
+        {
+            "img": torch.tensor(
+                [[[[1.6566806, 7.695548], [7.4342523, 13.580086]], [[11.776854, 18.669481], [18.396517, 21.551771]]]]
+            ),
+            "seg": torch.tensor([[[[1.0, 11.0], [7.0, 17.0]], [[9.0, 19.0], [15.0, 25.0]]]]),
+        },
+    ],
+    [
+        {
+            "keys": ("img", "seg"),
+            "mode": ["bilinear", InterpolationMode.NEAREST],
+            "magnitude_range": (0.3, 0.3),
+            "sigma_range": (1.0, 2.0),
+            "prob": 0.9,
+            "rotate_range": [1, 1, 1],
+            "as_tensor_output": True,
+            "device": torch.device("cpu:0"),
+            "spatial_size": (2, 2, 2),
+        },
+        {"img": torch.arange(27).reshape((1, 3, 3, 3)), "seg": torch.arange(27).reshape((1, 3, 3, 3))},
+        {
+            "img": torch.tensor(
+                [[[[1.6566806, 7.695548], [7.4342523, 13.580086]], [[11.776854, 18.669481], [18.396517, 21.551771]]]]
+            ),
+            "seg": torch.tensor([[[[1.0, 11.0], [7.0, 17.0]], [[9.0, 19.0], [15.0, 25.0]]]]),
+        },
+    ],
+]
+
+
+class TestRand3DElasticdEnums(unittest.TestCase):
+    @parameterized.expand(TEST_CASES)
+    def test_rand_3d_elasticd(self, input_param, input_data, expected_val):
+        g = Rand3DElasticd(**input_param)
+        g.set_random_state(123)
+        res = g(input_data)
+        for key in res:
+            result = res[key]
+            expected = expected_val[key] if isinstance(expected_val, dict) else expected_val
+            self.assertEqual(torch.is_tensor(result), torch.is_tensor(expected))
+            if torch.is_tensor(result):
+                np.testing.assert_allclose(result.cpu().numpy(), expected.cpu().numpy(), rtol=1e-4, atol=1e-4)
+            else:
+                np.testing.assert_allclose(result, expected, rtol=1e-4, atol=1e-4)
+
+
+if __name__ == "__main__":
+    unittest.main()
